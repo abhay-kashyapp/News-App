3e31fe14556316948369fab0f2ab0171bf1a9f85
abhaykashyap <abhaykashyap2804@gmail.com>
2025-05-12 23:56:37 +0530

    updated

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Card = ({data}) => {
   //console.log(data);
 
-
   const readMore = (url) =>{
    window.open(url)
   }
--- src/Components/Newsapp.jsx
+++ src/Components/Newsapp.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import Card from './card';
-import { use } from 'react';
-
-
 
 const Newsapp = () => {
 
